453c6e1c03a9e9e87313b5f62181a65df4ecc36c
Angush (MacBook) <angus.stienlehmann@gmail.com>
2019-12-30 00:21:50 +1000

    Fixed filler element padding on category list

--- src/functions/padWithEmptyElements.js
+++ src/functions/padWithEmptyElements.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
 const padWithEmptyElements = (array, MAX_PER_ROW, className) => {
-  let needed = (1 - ((array.length / MAX_PER_ROW) % 1)) * MAX_PER_ROW
-  let arr = [needed]
-  return arr.map((item, index) => {
-    return <div className={className} key={index}></div>
-  })
+  let needed = 1 - ((array.length / MAX_PER_ROW) % 1)
+  needed = needed === 1 ? 0 : needed * MAX_PER_ROW
+  let elements = []
+  for (let el = 0; el < needed; el++) {
+    elements.push(
+      <div
+        className={className}
+        key={`${className.replace(/\s+/g, '-')}-${el}`}
+      ></div>
+    )
+  }
+  return elements
 }
 
 export default padWithEmptyElements
