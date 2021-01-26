d325a67ad7f51e0e73fb3917094047bb8feb1956
Angush <angus.stienlehmann@gmail.com>
2021-01-26 19:10:59 +1000

    ADD: authorization variant on 404 page

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Link } from '@reach/router'
+import { Link, useMatch } from '@reach/router'
 
 const NotFoundPage = ({ links }) => {
+  const match = useMatch(`/unauthorized`)
+
   return (
     <div className='mx-auto text-center' style={{ 
       width: 'fit-content',
       marginTop: '20vh' 
     }}>
-      <h4>Page not found!</h4>
-      <h4>
-        <small className='text-muted'>Try one of these...</small>
+      <h4 style={{ marginBottom: '1rem' }}>{match ? "You're not authorized!" : "Page not found!"}</h4>
+      <h4 style={{ marginBottom: '24px' }}>
+        <small className='text-muted'>Sorry. Try one of these...</small>
       </h4>
       {links.map((link, index) => (
         <Link to={link.to} key={index} style={{ margin: '12px' }}>
