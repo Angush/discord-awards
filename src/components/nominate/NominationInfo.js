8edae2a2f70734ddc43dfd42ae8537c7ceba087b
Angus H. Young <angus.stienlehmann@gmail.com>
2023-01-05 01:50:49 +1000

    tweak: modified NominationInfo text

--- src/components/nominate/NominationInfo.js
+++ src/components/nominate/NominationInfo.js
@@ -28,7 +28,7 @@ const NominationInfo = () => {
               Entrants must be a fanwork for a Wildbow universe (ie. Parahumans,
               Otherverse, Twig) or otherwise associated with the fandom ("The
               creator is part of the fandom" doesn't count), except where
-              otherwise indicated.
+              otherwise indicated or not applicable.
             </li>
             <li>
               Entrants must have been initially posted or updated in a
@@ -39,25 +39,31 @@ const NominationInfo = () => {
             <li>
               Entrants may be created by any persons, on or off Cauldron, except
               where otherwise indicated (eg. "by a Cauldron member" in a
-              category title or description).
+              category title, description, or embedded criteria/info accordion
+              or modal).
             </li>
             <li>
               Entrants must be adherent to the rules of the Cauldron Discord —
-              chiefly, rules 5 (no racism, sexism, homo- or trans-phobia, etc.)
-              and 6 (no sexual content involving minor characters or sexual
-              assault, etc.) If you don't know where the full text for these
-              rules are, you shouldn't be nominating anything.
+              chiefly, <strong>rule 5</strong> (no racism, sexism, homo- or
+              trans-phobia, etc.) and <strong>rule 6</strong> (no sexual content
+              involving minor characters or sexual assault, etc.) If you don't
+              know where the full text for these rules are, you shouldn't be
+              nominating anything.
             </li>
             <li>
               Wherever a category indicates that additional criteria
               stipulations apply (whether in its title, description, or the
-              embedded criteria accordions or modals), entrants must also
+              embedded criteria/info accordions or modals), entrants must also
               satisfy those criteria.
             </li>
             <li>
-              Self-nominations (ie. nominating your own creations) are permitted
-              and encouraged — you're the least likely person to forget to
-              nominate your work!
+              Self-nominations (ie. nominating your own creations) are{' '}
+              <strong>
+                permitted and <em>encouraged</em>
+              </strong>
+              . We don't want great works to slip through the cracks, and{' '}
+              <em>you're</em> the least likely person to forget to nominate your
+              own works!
             </li>
           </ol>
           <p>
@@ -65,6 +71,14 @@ const NominationInfo = () => {
             because it's something <em>you</em> made, or you think someone else
             might have already nominated what <em>you</em> want to nominate —
             it's better to have a duplicate entry than no entry at all!
+          </p>
+          <p>
+            Similarly, if a category has additional criteria or information, and
+            you're unsure whether or not your nominee meets the requirements:
+            just nominate it anyway! Non-qualifying entrants will be rejected
+            during the vetting process after nominations are finished. It's
+            better to have 10 rejected entrants and 10 accepted than only 5
+            entrants in total.
           </p>
         </div>
       </details>
