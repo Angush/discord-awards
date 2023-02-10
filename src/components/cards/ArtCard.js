43e9233be51c8f3b5111da5c73a26ffeb5af8659
Angus H. Young <angus.stienlehmann@gmail.com>
2023-02-10 18:12:34 +1000

    fix: support youtube shorts links for embedding

--- src/components/cards/ArtCard.js
+++ src/components/cards/ArtCard.js
@@ -8,11 +8,15 @@ import FicLinks from './FicLinks'
 
 const getEmbed = (url, data) => {
   const title = `${data.title || 'Untitled'} by ${data.artist || 'Unknown'}`
-  const match = url.match(/(youtube.com\/watch\?v=|youtu.be\/)(?<id>[\w-]+)/)
+  const match = url.match(
+    /(youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/shorts\/)(?<id>[\w-]+)/
+  )
   if (match?.groups?.id)
     return <YouTubeEmbed id={match.groups.id} title={title} />
   if (url.match(/redd\.it.+format=mp4/))
     return <GenericVideoEmbed url={url} title={title} />
+
+  return
 }
 
 const ArtCard = ({
