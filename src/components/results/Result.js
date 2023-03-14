71b00e43cb6fad4b3ecc7d3d7886e6ac0424d6d6
Angus H. Young <angus.stienlehmann@gmail.com>
2023-03-14 11:15:20 +1000

    fix: typo in previous commit

--- src/components/results/Result.js
+++ src/components/results/Result.js
@@ -142,7 +142,7 @@ const Result = ({
       {blur && !image && title && indicator}
       {votedFor && <h6 className='voted-for'>You voted for this.</h6>}
       {votePercentage}
-      {entry.placement && entry.category === 'fic' && (
+      {entry.placement && category.type === 'fic' && (
         // badges only generated for fic categories at present
         <BadgeBox year={year} category={category} entry={entry} />
       )}
